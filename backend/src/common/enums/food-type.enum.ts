a51e52d8ff95ef6dc72507a3dce4627ad84fa364
Uzumaki-Cutehihi <160825380+Uzumaki-Cutehihi@users.noreply.github.com>
2026-03-25 09:50:55 +0700

    done enum

---
+++ backend/src/common/enums/food-type.enum.ts
@@ -0,0 +1,11 @@
+export enum FoodType {
+  INGREDIENT = 'ingredient',
+  DISH = 'dish',
+  PRODUCT = 'product',
+}
+
+/*
+    - INGREDIENT: Thành phần nguyên liệu, ví dụ: gạo, thịt, rau củ, ...
+    - DISH: Món ăn đã được chế biến, ví dụ: cơm tấm, phở bò, ...
+    - PRODUCT: Sản phẩm đóng gói, ví dụ: mì ăn liền, bánh mì, ...
+*/
