e3ca6d8a6f47b334a60947d4502a00aa9d1d5d1e
Uzumaki-Cutehihi <160825380+Uzumaki-Cutehihi@users.noreply.github.com>
2026-05-19 20:02:24 +0700

    ok

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.22")
     }
 }
@@ -16,4 +16,8 @@ allprojects {
         google()
         mavenCentral()
     }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
